ff7107127735e3a7716a50dd272e6f48efdcc21e
Angel Daniel Sanchez Garcia <adanielsanchezgarcia@gmail.com>
2024-12-17 19:53:50 +0100

    FEAT: install and run MT4

---
+++ start/install_mt4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+installer="/config/.wine/drive_c/mt4.exe"
+
+winecfg -v=win10
+curl -o "$installer" "https://download.mql5.com/cdn/web/metaquotes.software.corp/mt4/mt4oldsetup.exe"
+exec wine "$installer" /auto & wait
--- start/start.sh
+++ start/start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-touch /asdf
+mt4exe='/config/.wine/drive_c/Program Files (x86)/MetaTrader 4/terminal.exe'
+
+if [ ! -e "$mt4exe" ]; then
+	bash /start/install_mt4.sh
+fi
+
+if [ -e "$mt4exe" ]; then
+	echo "MT4 is installed"
+	wine "$mt4exe"
+else
+	echo "File {$mt4exe} does not exist. Installation error. MT4 can't run."
+fi
