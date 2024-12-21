e1a8e992045d33ce6ed05cd5653c0dde94b6e168
Angel Daniel Sanchez Garcia <adanielsanchezgarcia@gmail.com>
2024-12-21 01:02:12 +0100

    FIX: delete the installation executable when done using it

--- start/install_mt4.sh
+++ start/install_mt4.sh
@@ -5,3 +5,4 @@ installer="/config/.wine/drive_c/mt4.exe"
 winecfg -v=win10
 curl -o "$installer" "https://download.mql5.com/cdn/web/metaquotes.software.corp/mt4/mt4oldsetup.exe"
 exec wine "$installer" /auto & wait
+rm -f "$installer"
